a0ddd1cb0a69d1c0aa940b88f0d37767d5c5ab9d
Omanomally <solitromarco@gmail.com>
2020-04-28 20:56:03 -0500

    Patch 1.0.2

--- app/src/main/java/jolt/pack/MainActivity.kt
+++ app/src/main/java/jolt/pack/MainActivity.kt
@@ -2,14 +2,10 @@ package jolt.pack
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +15,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigation(){
+
         val navController = findNavController (R.id.nav_host_fragment)
-        setupActionBarWithNavController(navController)
-
         val bottomNav = findViewById<BottomNavigationView>(R.id.botNav)
-        bottomNav.setupWithNavController(navController)
+        bottomNav?.setupWithNavController(navController)
 
-        setupActionBarWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
-
-
-    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }*/
 }
--- app/src/main/java/jolt/pack/Settings.kt
+++ app/src/main/java/jolt/pack/Settings.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FirstFragment : Fragment() {
+class Settings : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
