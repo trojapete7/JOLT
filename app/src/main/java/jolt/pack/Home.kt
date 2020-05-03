f22947957948b926ce1159a58bf6ff90428922dd
Omanomally <solitromarco@gmail.com>
2020-05-02 20:46:36 -0500

    Work in Progress 05/02/2020

--- app/src/main/java/jolt/pack/Home.kt
+++ app/src/main/java/jolt/pack/Home.kt
@@ -12,6 +12,7 @@ import android.widget.Button
 import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_home.*
 
+
 const val REQUEST_ENABLE_BT = 1
 const val MODULE_MAC = "98:D3:B1:FD:71:35"
 private const val SCAN_PERIOD: Long = 10000
@@ -19,30 +20,50 @@ private const val SCAN_PERIOD: Long = 10000
 class Home : Fragment() {
 
     override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //val rootView: View = inflater.inflate(R.layout.fragment_home, container, false)
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
         view.findViewById<Button>(R.id.bt_connection_butt).setOnClickListener {
             onConnectClick()
         }
 
-        /*val hoursIn = view.findViewById<EditText>(R.id.editHours)
-        val minutesIn = view.findViewById<EditText>(R.id.editMinutes)
-        val secondsIn = view.findViewById<EditText>(R.id.editSeconds)*/
-
         view.findViewById<Button>(R.id.start_monitor_butt).setOnClickListener {
-            val hours = view.findViewById<EditText>(R.id.editHours).text.toString().toInt()
-            val minutes = view.findViewById<EditText>(R.id.editMinutes).text.toString().toInt()
-            val seconds = view.findViewById<EditText>(R.id.editSeconds).text.toString().toInt()
-            setupTimer(hours, minutes, seconds)
+            val minutes = view.findViewById<EditText>(R.id.editMinutes).text.toString()
+            val seconds = view.findViewById<EditText>(R.id.editSeconds).text.toString()
+
+            val numMinutes = cleanMinutes(minutes)
+            val numSeconds = cleanSeconds(seconds)
+
+            hideUi()
+            errMess.visibility = View.VISIBLE
+
+            val beginVal: Long = ((numMinutes*60*1000)+(numSeconds*1000)).toLong()
+            timer(beginVal, 1000).start()
+
         }
+
+        view.findViewById<Button>(R.id.cancel_monitor_butt).setOnClickListener {
+            errMess.visibility = View.INVISIBLE
+            makeUiVisible()
+        }
+
         return view
     }
 
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }*/
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
 
     // Might need onActivityResult() here to catch the result from startActivityForResult() method
     private fun onConnectClick (){
@@ -52,44 +73,106 @@ class Home : Fragment() {
             errMess.text = getString(R.string.err_message_no_bt)
         }
 
-        /*if (bluetoothAdapter?.isEnabled == false){
+        if (bluetoothAdapter?.isEnabled == false){
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
 
             //ADD BT CONNECTIVITY STUFF HERE
 
-        }*/
+        }
     }
 
-    private fun setupTimer(hours: Int, minutes: Int, seconds: Int){
-        val beginVal = (hours*60*60*1000)+(minutes*60*1000)+(seconds*1000)
-        val beginValLong: Long = beginVal.toLong()
+    private fun timer(millisUntilFinished: Long, countDownInterval: Long):CountDownTimer {
+        return object:CountDownTimer(millisUntilFinished, countDownInterval){
 
-        val cdTimer = object:CountDownTimer(beginValLong,1000){
             override fun onTick(millisUntilFinished: Long){
+
                 var difference = millisUntilFinished
                 val secondsInMilli: Long = 1000
                 val minutesInMilli = secondsInMilli * 60
-                val hoursInMilli = minutesInMilli * 60
+                //val hoursInMilli = minutesInMilli * 60
 
-                val elapsedHours = difference / hoursInMilli
-                difference %= hoursInMilli
+                //val elapsedHours = difference / hoursInMilli
+                //difference %= hoursInMilli
 
                 val elapsedMinutes = difference / minutesInMilli
                 difference %= minutesInMilli
 
                 val elapsedSeconds = difference / secondsInMilli
 
-                editHours.setText((elapsedHours).toInt().toString())
-                editMinutes.setText((elapsedMinutes).toInt().toString())
-                editSeconds.setText((elapsedSeconds).toInt().toString())
-            }
-            override fun onFinish(){
-                errMess.text = getString(R.string.timer_done)
+                //val displayHours = ((elapsedHours).toInt().toString())
+                val displayMinutes = ((elapsedMinutes).toInt().toString())
+                val displaySeconds = ((elapsedSeconds).toInt().toString())
+
+                errMess.text = ("$displayMinutes : $displaySeconds")
             }
 
+            override fun onFinish(){
+                makeUiVisible()
+                errMess.text = ""
+            }
         }
-        cdTimer.start()
+    }
+
+    private fun makeUiVisible() {
+        editMinutes.visibility = View.VISIBLE
+        editSeconds.visibility = View.VISIBLE
+        separatorMinutesToSeconds.visibility = View.VISIBLE
+        bt_connection_butt.visibility = View.VISIBLE
+        start_monitor_butt.visibility = View.VISIBLE
+        (activity as MainActivity).SetNavigationVisibilty(true)
+
+        cancel_monitor_butt.visibility = View.INVISIBLE
+    }
+
+    private fun hideUi() {
+        editMinutes.visibility = View.INVISIBLE
+        editSeconds.visibility = View.INVISIBLE
+        separatorMinutesToSeconds.visibility = View.INVISIBLE
+        bt_connection_butt.visibility = View.INVISIBLE
+        start_monitor_butt.visibility = View.INVISIBLE
+        (activity as MainActivity).SetNavigationVisibilty(false)
+        cancel_monitor_butt.visibility = View.VISIBLE
+    }
+
+    private fun cleanMinutes(m:String):Int {
+        val checkMinutes: String
+
+        if (m.isEmpty()){
+            checkMinutes = "0"
+            return checkMinutes.toInt()
+        }
+
+        val minutes: CharArray = m.toCharArray()
+        val trueM: Int
+
+        trueM = if (minutes[0] == '0'){
+            minutes[1].toString().toInt()
+        } else {
+            minutes.joinToString(separator="").toInt()
+        }
+        return trueM
+    }
+
+    private fun cleanSeconds(s:String):Int {
+        val checkSeconds: String
+
+        if (s.isEmpty()){
+            checkSeconds = "0"
+            return checkSeconds.toInt()
+        }
+        val seconds: CharArray = s.toCharArray()
+        val trueS: Int
+
+        trueS = if (seconds[0] == '0') {
+            seconds[1].toString().toInt()
+        } else {
+            seconds.joinToString(separator="").toInt()
+        }
+        return trueS
     }
 
 }
+
+
+
--- app/src/main/java/jolt/pack/MainActivity.kt
+++ app/src/main/java/jolt/pack/MainActivity.kt
@@ -1,51 +1,35 @@
 package jolt.pack
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var currentNavController: LiveData<NavController>? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        } // Else, need to wait for onRestoreInstanceState
+        setupNavigation()
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        setupBottomNavigationBar()
+    private fun setupNavigation(){
+
+        val navController = findNavController (R.id.nav_host_fragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav?.setupWithNavController(navController)
+
     }
 
-    private fun setupBottomNavigationBar() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+    fun SetNavigationVisibilty(b: Boolean){
+        if(b){
+            bottom_nav.visibility = View.VISIBLE
+        }
 
-        val navGraphIds = listOf(R.navigation.calendar, R.navigation.rewards, R.navigation.home, R.navigation.schedule, R.navigation.settings)
-
-        // Setup the bottom navigation view with a list of navigation graphs
-        val controller = bottomNavigationView.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.nav_host_container,
-            intent = intent
-        )
-
-        // Whenever the selected controller changes, setup the action bar.
-        controller.observe(this, Observer { navController ->
-            setupActionBarWithNavController(navController)
-        })
-        currentNavController = controller
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return currentNavController?.value?.navigateUp() ?: false
+        else {
+            bottom_nav.visibility = View.GONE
+        }
     }
 }
