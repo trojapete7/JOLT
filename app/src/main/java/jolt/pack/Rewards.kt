faeca28f97dd31732a9bf1d52ebfc1183dc3acdb
Troy Peters <troy.peters@ttu.edu>
2020-05-03 23:20:13 -0600

    my rewards tab

--- app/src/main/java/jolt/pack/Rewards.kt
+++ app/src/main/java/jolt/pack/Rewards.kt
@@ -13,6 +13,11 @@ const val lyft_coupon: Int = 750
 const val big_mac: Int = 500
 const val wendys_frosty: Int = 200
 
+var num_day_passes: Int = 0
+var num_lyft: Int = 0
+var num_big_macs: Int = 0
+var num_frosty: Int = 0
+
 class Rewards : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -21,21 +26,31 @@ class Rewards : Fragment() {
 
         view.findViewById<Button>(R.id.six_flags_button).setOnClickListener {
             currentPoints -= six_flags
+            num_day_passes++
             user_points.text = currentPoints.toString()
         }
 
         view.findViewById<Button>(R.id.lyft_button).setOnClickListener {
             currentPoints -= lyft_coupon
+            num_lyft++
             user_points.text = currentPoints.toString()
         }
 
         view.findViewById<Button>(R.id.big_mac_button).setOnClickListener {
             currentPoints-= big_mac
+            num_big_macs++
             user_points.text = currentPoints.toString()
         }
 
         view.findViewById<Button>(R.id.frosty_button).setOnClickListener {
             currentPoints -= wendys_frosty
+            num_frosty++
+            user_points.text = currentPoints.toString()
+        }
+
+        view.findViewById<Button>(R.id.rewards_button).setOnClickListener {
+            currentPoints -= wendys_frosty
+            num_frosty++
             user_points.text = currentPoints.toString()
         }
 
