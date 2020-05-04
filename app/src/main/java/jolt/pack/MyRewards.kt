bd41032582ca82ef224587c6a3c381165bb3af91
Troy Peters <troy.peters@ttu.edu>
2020-05-03 23:22:23 -0600

    my rewards tab

---
+++ app/src/main/java/jolt/pack/MyRewards.kt
@@ -0,0 +1,17 @@
+package jolt.pack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_rewards.*
+
+class MyRewards: Fragment() {
+
+}
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view: View = inflater.inflate(R.layout.fragment_my_rewards, container, false)
+        
+    }
