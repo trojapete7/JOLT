778dc4a0017dc690b32d56ea692cdc91f00d03a4
Omanomally <solitromarco@gmail.com>
2020-05-03 22:27:50 -0500

    Rewards in progress

--- app/src/main/java/jolt/pack/Home.kt
+++ app/src/main/java/jolt/pack/Home.kt
@@ -15,30 +15,54 @@ import android.widget.Button
 import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_home.*
 import java.io.IOException
-import java.time.LocalDate
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 import java.util.*
+import jolt.*
 
-
+//Request code for intent to start BT service
 const val REQUEST_ENABLE_BT = 1
+
+//MAC Address for HC-05 BT Module
 const val MODULE_MAC = "98:D3:B1:FD:71:35"
+
+//Serial Port Protocol UUID
 val UUID: UUID = java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+//Period to scan for available BT devices
 private const val SCAN_PERIOD: Long = 10000
+
+//Initializing bluetooth adapter object with available BT adapter in device
+//Can be null
 val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+//Flag for seeing if the timer or monitoring is currently running or not
 var isTimerRunning: Boolean = false
+
+//Initializing connected thread object for the BT connection
 var btt: ConnectedThread? = null
 
+//Flag for how many times user interrupts monitoring
+var userUnlock: Int = 0
+
+//String to be sent to Arduino board to control LED "vibrations"
 const val vibrate: String = "1"
 
+var currentPoints: Int = 500
+
+/**CLASS FOR THE HOME/MONITORING FRAGMENT. THE MAIN SCREEN OF THE JOLT APPLICATION.**/
 class Home : Fragment() {
 
     var durationInMillis  = 0L
+    //Method to get duration user has set for monitoring mode
     private fun getDuration (Long: Long):Long {
         durationInMillis = Long
         return Long
     }
 
+    /**
+    *  Sets listeners for all button objects within the fragment_home.xml layout
+    *  Calls corresponding functions and performs various other related actions
+    *  such as setting UI visibility, setting text size,  setting timer flag, etc.
+    **/
     override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
@@ -67,43 +91,17 @@ class Home : Fragment() {
 
         }
 
-        //Cancel Monitoring button actions
-        view.findViewById<Button>(R.id.cancel_monitor_butt).setOnClickListener {
-            errMess.visibility = View.INVISIBLE
-            makeUiVisible()
-        }
-
         return view
     }
 
-    //var beforeTime = System.currentTimeMillis()
-    //var afterTime = System.currentTimeMillis()
-    var userUnlock: Int = 0
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        //beforeTime = System.currentTimeMillis()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
+    /**
+     *  Captures when the user unlocks their phone screen while monitoring mode is active
+     *  Only increments X for user interruption during monitoring mode.
+     *  Sends the corresponding message through the BT connection to interact with the LED "vibration"
+     *  in order to alert the user that they need to refocus.
+     **/
     override fun onResume() {
         super.onResume()
-        //afterTime = System.currentTimeMillis() - beforeTime
 
         if (isTimerRunning){
             userUnlock++
@@ -111,6 +109,13 @@ class Home : Fragment() {
         }
     }
 
+    /**
+     *  Function that is called whenever the "Connect to Device" button is clicked. If bluetoothAdapter
+     *  is null then the error message text view is updated to warn the user their device is
+     *  incompatible. Then checks if the bluetoothAdapter is enabled. If it is not enabled an intent
+     *  is created to ask the user to turn on the BT adapter. Otherwise, the adapter must already be
+     *  turned on so initiateBluetoothProcess is called.
+     **/
     private fun onConnectClick (){
 
         if (bluetoothAdapter == null){
@@ -128,6 +133,10 @@ class Home : Fragment() {
         }
     }
 
+    /**
+     *  This function is called after startActivityForResult() function is done. If the BT intent
+     *  to turn on the BT adapter succeeds then initiateBluetoothProcess() is called.
+     **/
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT){
@@ -135,7 +144,12 @@ class Home : Fragment() {
         }
     }
 
-     private fun initiateBluetoothProcess() {
+    /**
+     *  Function that sets up the Serial Port for BT connection data transfer. SPP used on an
+     *  RFcomm Socket to Service Record. This is the more secure version of the RFcomm implementation.
+     *  Not vulnerable to Man-In-The-Middle attacks.
+     **/
+    private fun initiateBluetoothProcess() {
         if (bluetoothAdapter != null) {
             if(bluetoothAdapter.isEnabled) {
                 var tmp: BluetoothSocket?
@@ -157,12 +171,6 @@ class Home : Fragment() {
                 val handlerThread = HandlerThread("HC-05_HandlerThread")
                 val mHandler = Handler(Looper.getMainLooper())
 
-                fun handleMessage(msg: Message){
-                    if(msg.what == ConnectedThread.RESPONSE_MESSAGE){
-                        val txt: String = msg.obj.toString()
-                    }
-                }
-
                 Log.i("[BLUETOOTH]", "Creating and running thread")
                 btt = ConnectedThread(mmSocket, mHandler)
                 btt!!.start()
@@ -171,6 +179,11 @@ class Home : Fragment() {
         }
     }
 
+    /**
+     *  Function to setup CountDownTimer object. Requires onTick() and onFinish() implementation.
+     *  onTick() runs every second and calculates what to display to the user. onFinish() runs once
+     *  the millisUntilFinished is equal to 0.
+     **/
     private fun timer(millisUntilFinished: Long, countDownInterval: Long):CountDownTimer {
         return object:CountDownTimer(millisUntilFinished, countDownInterval){
 
@@ -218,8 +231,6 @@ class Home : Fragment() {
         bt_connection_butt.visibility = View.VISIBLE
         start_monitor_butt.visibility = View.VISIBLE
         (activity as MainActivity).setNavigationVisibility(true)
-
-        cancel_monitor_butt.visibility = View.INVISIBLE
     }
 
     private fun hideUi() {
@@ -229,9 +240,13 @@ class Home : Fragment() {
         bt_connection_butt.visibility = View.INVISIBLE
         start_monitor_butt.visibility = View.INVISIBLE
         (activity as MainActivity).setNavigationVisibility(false)
-        cancel_monitor_butt.visibility = View.VISIBLE
     }
 
+    /**
+     *  cleanMinutes() and cleanSeconds() takes care of input validation. If user enters nothing
+     *  into the timer box and starts the timer will not crash the application. Will simply set the
+     *  timer to 0 milliseconds causing it to call onFinish() from the CountDownTimer object immediately.
+     **/
     private fun cleanMinutes(m:String):Int {
         val checkMinutes: String
 
@@ -269,6 +284,11 @@ class Home : Fragment() {
         return trueS
     }
 
+    /**
+     *  Calculates the total points earned per session. Is called whenever the user finishes a
+     *  monitoring session. Also sets a text view to display the number of points earned and the
+     *  number of times the user interrupted the monitoring.
+     **/
     private fun pointsEarned(){
         //val timeEarned = (durationInMillis - afterTime)/1000
         val secondsEarned = (durationInMillis)/1000
@@ -278,12 +298,8 @@ class Home : Fragment() {
         if (totalPoints < 10){
             totalPoints = 10
         }
-
         points.text = "You earned $totalPoints points and unlocked: $userUnlock time(s)"
+        currentPoints += totalPoints.toInt()
     }
 
-
 }
-
-
-
--- app/src/main/java/jolt/pack/Rewards.kt
+++ app/src/main/java/jolt/pack/Rewards.kt
@@ -5,17 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_rewards.*
+
+const val six_flags: Int = 2000
+const val lyft_coupon: Int = 750
+const val big_mac: Int = 500
+const val wendys_frosty: Int = 200
 
 class Rewards : Fragment() {
 
-    override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_rewards, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return rootView
+        val view: View = inflater.inflate(R.layout.fragment_rewards, container, false)
+
+        view.findViewById<Button>(R.id.six_flags_button).setOnClickListener {
+            currentPoints -= six_flags
+            user_points.text = currentPoints.toString()
+        }
+
+        view.findViewById<Button>(R.id.lyft_button).setOnClickListener {
+            currentPoints -= lyft_coupon
+            user_points.text = currentPoints.toString()
+        }
+
+        view.findViewById<Button>(R.id.big_mac_button).setOnClickListener {
+            currentPoints-= big_mac
+            user_points.text = currentPoints.toString()
+        }
+
+        view.findViewById<Button>(R.id.frosty_button).setOnClickListener {
+            currentPoints -= wendys_frosty
+            user_points.text = currentPoints.toString()
+        }
+
+        return view
     }
 
-   /* override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        user_points.text = currentPoints.toString()
+    }
 
-    }*/
 }
